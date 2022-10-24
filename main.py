17bcaa6deec129a1af183b4d3c576d2ff11dec2f
keerthana-mk <you@example.com>
2022-10-23 20:56:45 -0400

    adding cors middleware layer

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
@@ -10,7 +11,7 @@ from services.doctor_services import DoctorService
 from services.Profile.profiles_services import ProfileServices
 from models.commons import convert_patient_reponse, convert_doctor_response, convert_insurer_response, \
     get_http_response, StandardHttpResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 logger = logging.getLogger()
@@ -33,7 +34,19 @@ def create_tables():  # new
 
 def start_application():
     app = FastAPI(title="Vydhya", version="v1")
-    # create_tables()
+    origins = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+   "https://vydhya.netlify.app/",
+    "http://localhost",
+    "http://localhost:8080",
+    ]
+    app.add_middleware( CORSMiddleware,
+                        allow_origins=["*"],
+                        allow_credentials=True,
+                        allow_methods=["*"],
+                        allow_headers=["*"],
+                        )
     return app
 
 
@@ -55,19 +68,126 @@ def create_all_tables():
     create_tables()
 
 
-def create_tables():  # new
-    print("Creating tables")
-    Base.metadata.create_all(bind=engine)
-
-
-def start_application():
-    app = FastAPI(title="Vydhya", version="v1")
-    create_tables()
-    return app
-
-
-app = start_application()
-
 @app.get("/")
 async def root():
+    # return check_db_connected()
     return {"message": "Hello World"}
+
+
+@app.post("/user_registration", response_model=StandardHttpResponse, tags=['User Registration and Login'],
+          response_model_exclude_none=True)
+def create_user(user_details: UserRegistration):
+    auth_service = BaseAuthentication.get_auth_service()
+    error_message, data = None, None
+    try:
+        auth_service.add_user(user_details)
+        data = UserRegistrationResponse(
+            message=f'successfully created user {user_details.user_id}',
+            status_code=200
+        )
+        status = 200
+    except Exception as e:
+        error_message = f'failed to register user: {str(e)}'
+
+    return JSONResponse(get_http_response(data, status, error_message), status_code=status)
+
+
+@app.post("/login", response_model=StandardHttpResponse, tags=['User Registration and Login'],
+          response_model_exclude_none=True)
+def login_user(user_login_req: UserLoginRequest):
+    auth_service = BaseAuthentication.get_auth_service()
+    error_message, data = None, None
+    try:
+        data = auth_service.verify_user(user_login_req.user_id, user_login_req.user_password)
+        status = 200
+    except Exception as e:
+        error_message = f'error while authenticating user {user_login_req.user_id}: {str(e)}'
+        status = 500
+    return JSONResponse(content=get_http_response(data, status, error_message), status_code=status)
+
+
+# @app.post('/login/google', response_model=UserLoginResponse, tags=['User Registration and Login'])
+# async def login_user1(request: Request):
+#     redirect_uri = request.url_for('auth')
+#     return await goauth.google.authorize_access_token(request, redirect_uri)
+
+# @app.post('/token', response_model= Token)
+# def
+
+@app.post("/profile", response_model=StandardHttpResponse, tags=['User Profiles'], response_model_exclude_none=True)
+def update_user_profile(user_id, user_role, user_profile: UserProfileRequests):
+    profile_service = ProfileServices()
+    error_message, data = None, None
+    try:
+        if user_role == 'patient':
+            updated_user_profile = profile_service.update_user_profile(user_id, user_role, user_profile.patient)
+            data = convert_patient_reponse(updated_user_profile)
+            status = 200
+        elif user_role == 'doctor':
+            updated_user_profile = profile_service.update_user_profile(user_id, user_role, user_profile.doctor)
+            data = convert_doctor_response(updated_user_profile)
+            status = 200
+        elif user_role == 'insurer':
+            updated_user_profile = profile_service.update_user_profile(user_id, user_role, user_profile.insurer)
+            data = convert_insurer_response(updated_user_profile)
+            status = 200
+        else:
+            error_message = f'unsupported user_role: {user_role}'
+            status = 500
+    except Exception as e:
+        error_message = f'error while authenticating user {user_id}: {str(e)}'
+        status = 500
+    return JSONResponse(get_http_response(data, status, error_message), status_code=status)
+
+
+@app.get("/profile", response_model=StandardHttpResponse, tags=['User Profiles'], response_model_exclude_none=True)
+def get_user_profiles(user_id, user_role):
+    logger.info("please tell me i am here")
+    profile_service = ProfileServices()
+    error_message, data = None, None
+    try:
+        if user_role == 'patient':
+            user_profile_details = profile_service.get_user_profile(user_id, user_role)
+            data = convert_patient_reponse(user_profile_details)
+            status = 200
+        elif user_role == 'doctor':
+            user_profile_details = profile_service.get_user_profile(user_id, user_role)
+            data = convert_doctor_response(user_profile_details)
+            status = 200
+        elif user_role == 'insurer':
+            user_profile_details = profile_service.get_user_profile(user_id, user_role)
+            data = convert_insurer_response(user_profile_details)
+            status = 200
+        else:
+            error_message = f'unsupported user_role: {user_role}'
+            status = 500
+    except Exception as e:
+        error_message = f'error while authenticating user {user_id}: {str(e)}'
+        logger.error(error_message)
+        status = 500
+
+    return JSONResponse(content=get_http_response(data, status, error_message), status_code=status)
+
+
+@app.post("/doctor/search", response_model=StandardHttpResponse, tags=['Search Doctor'],
+          response_model_exclude_none=True)
+def search_doctor(search_doctor_request: SearchDoctorRequest):
+    # if user_role not in ['patient', 'doctor', 'insurer']:
+    #     status = 400
+    #     error_message = f'unsupported role: {user_role}'
+    #     return JSONResponse(content=get_http_response(None, status, error_message), status_code=status)
+
+    data, error_message = None, None
+    try:
+        data = DoctorService.search_doctor(search_doctor_request.search_by, search_doctor_request.search_string,
+                                           search_doctor_request.covid_support)
+        status = 200
+    except BaseException as e:
+        error_message = f'error while searching doctors: {str(e)}'
+        logger.error(error_message)
+        status = 500
+    except BaseException as e:
+        error_message = f'error while searching doctors: {str(e)}'
+        logger.error(error_message)
+        status = 500
+    return JSONResponse(content=get_http_response(data, status, error_message), status_code=status)
