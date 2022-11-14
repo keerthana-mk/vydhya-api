5b9df1c7c6cd15a87365c62f6a6a2ccd38532383
keerthana-mk <you@example.com>
2022-11-14 18:22:49 -0500

    Fixed inbetween transaction fails

--- databases/repository/profiles.py
+++ databases/repository/profiles.py
@@ -48,15 +48,19 @@ class DoctorProfileRepository:
 
     @staticmethod
     def create_user_profile(user_id, user_email, user_role, theme):
+        try:
+            new_user_profile = DoctorProfile(
+                user_id=user_id,
+                contact_email=user_email,
+                theme=theme
+            )
+            DoctorProfileRepository.database.add(new_user_profile)
+            DoctorProfileRepository.database.commit()
+        except Exception as e:
+            DoctorProfileRepository.database.rollback()
+            error_message = "Error while creating doctor profile :{}".format(e)
+            raise(error_message)
         
-        new_user_profile = DoctorProfile(
-            user_id=user_id,
-            contact_email=user_email,
-            theme=theme
-        )
-        DoctorProfileRepository.database.add(new_user_profile)
-        DoctorProfileRepository.database.commit()
-
     @staticmethod
     def get_doctor_profile(user_id):
         doctor_query = DoctorProfileRepository.database.query(DoctorProfile).filter((DoctorProfile.user_id == user_id))
@@ -71,6 +75,7 @@ class DoctorProfileRepository:
                 user_profile_details)
             DoctorProfileRepository.database.commit()
         except Exception as e:
+            DoctorProfileRepository.database.rollback()
             raise BaseException(e)
 
     @staticmethod
@@ -101,15 +106,20 @@ class InsurerProfileRepository:
 
     @staticmethod
     def create_user_profile(insurer_id, user_id, user_email, user_role, theme):
-        new_user_profile = InsurerProfile(
-            insurer_id=insurer_id,
-            user_id=user_id,
-            contact_email=user_email,
-            theme=theme
-        )
-        InsurerProfileRepository.database.add(new_user_profile)
-        InsurerProfileRepository.database.commit()
-
+        try:
+            new_user_profile = InsurerProfile(
+                insurer_id=insurer_id,
+                user_id=user_id,
+                contact_email=user_email,
+                theme=theme
+            )
+            InsurerProfileRepository.database.add(new_user_profile)
+            InsurerProfileRepository.database.commit()
+        except Exception as e:
+            InsurerProfileRepository.database.rollback()
+            error_message = "Error while creating Insurer Profile: {}".format(e)
+            raise(error_message)
+        
     @staticmethod
     def get_insurer_profile(user_id):
         insurer_query = InsurerProfileRepository.database.query(InsurerProfile).filter(
@@ -125,4 +135,5 @@ class InsurerProfileRepository:
                 user_profile_details)
             InsurerProfileRepository.database.commit()
         except Exception as e:
+            InsurerProfileRepository.database.rollback()
             raise BaseException(e)
